b1f8527cf5871ade12251638e16d26c7ddc748d0
nexisn4 <nexisn4@gmail.com>
2023-04-12 15:07:34 -0500

    remove nav from index. update placeholder to use onload

--- js/placeholders.js
+++ js/placeholders.js
@@ -1,20 +1,17 @@
+var allow_scroll = true;
+
 $( document ).ready(function() {
     $("#nav-placeholder").load("templates/nav.html");
     $("#footer-placeholder").load("templates/footer.html");
-    // console.log(allow_scroll);
+
+    $('#nav-wrap').click(function(e) {
+        if (allow_scroll) {
+            $('body').css('overflow', 'hidden');
+        } else {
+            $('body').css('overflow', 'auto');
+        };
+        allow_scroll = !allow_scroll;
+    });
 });
 
 
-var allow_scroll = true;
-$('#nav-wrap').click(function(e) {
-    console.log(allow_scroll);
-
-    if (allow_scroll) {
-        $('body').css('overflow', 'hidden');
-    } else {
-        $('body').css('overflow', 'auto');
-    };
-
-    allow_scroll = !allow_scroll;
-    console.log(allow_scroll);
-});
