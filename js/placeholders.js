c3fb518b08ed3b7cfb11bfe109b5c9dcb42d9797
nexisn4 <nexisn4@gmail.com>
2023-04-12 15:01:57 -0500

    change icons and text outlines at homepage. Prevent scroll on page while nav is open.

--- js/placeholders.js
+++ js/placeholders.js
@@ -1,4 +1,20 @@
 $( document ).ready(function() {
     $("#nav-placeholder").load("templates/nav.html");
     $("#footer-placeholder").load("templates/footer.html");
+    // console.log(allow_scroll);
+});
+
+
+var allow_scroll = true;
+$('#nav-wrap').click(function(e) {
+    console.log(allow_scroll);
+
+    if (allow_scroll) {
+        $('body').css('overflow', 'hidden');
+    } else {
+        $('body').css('overflow', 'auto');
+    };
+
+    allow_scroll = !allow_scroll;
+    console.log(allow_scroll);
 });
