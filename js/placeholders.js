c6c52fad4e7405586d80c81c025948e3d33e5898
nexisn4 <nexisn4@gmail.com>
2023-04-12 21:48:53 -0500

    js

--- js/placeholders.js
+++ js/placeholders.js
@@ -1,25 +1,25 @@
 var allow_scroll = true;
 
 $( document ).ready(function() {
+    console.log("on load: ", allow_scroll);
+
     $("#nav-placeholder").load("templates/nav.html", function(){
-        // console.log('loaded nav');
+        console.log('loaded nav');
         $('.mobile-btn').click(function(e) {
-            // console.log('clicked menu. currently: ', allow_scroll);
+            console.log('clicked menu. currently: ', allow_scroll);
             if (allow_scroll) {
-                // $('body').css('overflow', 'hidden');
-                $('#nav-placeholder').css('touch-action', 'none');
+                $('body').css('overflow', 'hidden');
+                // $('#nav-placeholder').css('touch-action', 'none');
             } else {
-                // $('body').css('overflow', 'auto');
-                $('#nav-placeholder').css('touch-action', 'auto');
+                $('body').css('overflow', 'auto');
+                // $('#nav-placeholder').css('touch-action', 'auto');
             };
             allow_scroll = !allow_scroll;
-            // console.log("now: ", allow_scroll);
+            console.log("now: ", allow_scroll);
         });
     });
 
     $("#footer-placeholder").load("templates/footer.html");
 
     // $('#nav-placeholder').empty();
-    
-    // console.log("on load: ", allow_scroll);
 });
