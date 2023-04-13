e2a0fbad5dbc4487d806888262b86288272d3981
nexisn4 <nexisn4@gmail.com>
2023-04-12 21:10:11 -0500

    use settimeout on menu

--- js/placeholders.js
+++ js/placeholders.js
@@ -3,18 +3,18 @@ var allow_scroll = true;
 $( document ).ready(function() {
     $("#nav-placeholder").load("templates/nav.html");
     $("#footer-placeholder").load("templates/footer.html");
-
+    
     console.log("on load: ", allow_scroll);
-    $('.mobile-btn').click(function(e) {
-        console.log('clicked menu. currently: ', allow_scroll);
-        if (allow_scroll) {
-            $('body').css('overflow', 'hidden');
-        } else {
-            $('body').css('overflow', 'auto');
-        };
-        allow_scroll = !allow_scroll;
-        console.log("now: ", allow_scroll);
-    });
-
-    // $('#nav-placeholder').empty();
+    setTimeout(function(){
+        $('.mobile-btn').click(function(e) {
+            console.log('clicked menu. currently: ', allow_scroll);
+            if (allow_scroll) {
+                $('body').css('overflow', 'hidden');
+            } else {
+                $('body').css('overflow', 'auto');
+            };
+            allow_scroll = !allow_scroll;
+            console.log("now: ", allow_scroll);
+        });
+    }, 3000);
 });
