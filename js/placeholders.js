dfd38fdae75e52cbd8cac667406335be44a99928
nexisn4 <nexisn4@gmail.com>
2023-04-12 21:17:48 -0500

    js .load(callback)

--- js/placeholders.js
+++ js/placeholders.js
@@ -1,11 +1,9 @@
 var allow_scroll = true;
 
 $( document ).ready(function() {
-    $("#nav-placeholder").load("templates/nav.html");
-    $("#footer-placeholder").load("templates/footer.html");
-    
-    console.log("on load: ", allow_scroll);
-    setTimeout(function(){
+    $("#nav-placeholder").load("templates/nav.html", function(){
+        console.log('loaded nav');
+
         $('.mobile-btn').click(function(e) {
             console.log('clicked menu. currently: ', allow_scroll);
             if (allow_scroll) {
@@ -16,5 +14,16 @@ $( document ).ready(function() {
             allow_scroll = !allow_scroll;
             console.log("now: ", allow_scroll);
         });
+    });
+
+    $("#footer-placeholder").load("templates/footer.html", function(){
+        console.log('loaded foot');
+    });
+
+    // $('#nav-placeholder').empty();
+    
+    console.log("on load: ", allow_scroll);
+    setTimeout(function(){
+        
     }, 3000);
 });
