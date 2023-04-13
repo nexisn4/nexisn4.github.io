d38ac27d737c09aff16f387c5283c0eaa07068f4
nexisn4 <nexisn4@gmail.com>
2023-04-12 23:33:53 -0500

    bootstrap nav

--- js/placeholders.js
+++ js/placeholders.js
@@ -5,18 +5,18 @@ $( document ).ready(function() {
 
     $("#nav-placeholder").load("templates/nav.html", function(){
         console.log('loaded nav');
-        $('.mobile-btn').click(function(e) {
-            console.log('clicked menu. currently: ', allow_scroll);
-            if (allow_scroll) {
-                $('body').css('overflow', 'hidden');
-                // $('#nav-placeholder').css('touch-action', 'none');
-            } else {
-                $('body').css('overflow', 'auto');
-                // $('#nav-placeholder').css('touch-action', 'auto');
-            };
-            allow_scroll = !allow_scroll;
-            console.log("now: ", allow_scroll);
-        });
+        // $('.mobile-btn').click(function(e) {
+        //     console.log('clicked menu. currently: ', allow_scroll);
+        //     if (allow_scroll) {
+        //         $('body').css('overflow', 'hidden');
+        //         // $('#nav-placeholder').css('touch-action', 'none');
+        //     } else {
+        //         $('body').css('overflow', 'auto');
+        //         // $('#nav-placeholder').css('touch-action', 'auto');
+        //     };
+        //     allow_scroll = !allow_scroll;
+        //     console.log("now: ", allow_scroll);
+        // });
     });
 
     $("#footer-placeholder").load("templates/footer.html");
